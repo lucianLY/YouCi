ffc819e9bf4d163ff152d8c0afb9da5a4ad9fe9d
lucian <montreal.lucian@gmail.com>
2021-12-14 21:06:05 +0800

    update

--- src/view/Product/create.js
+++ src/view/Product/create.js
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from 'react'
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
+
+const code = 'const a = 0;';
+
+
 
 const Create = () => {
     return (
-        <div className="window">Category</div>
+        <div className="product">
+            <CodeMirror
+            value={'CodeMirror'}
+            options={{
+                theme: 'monokai',
+                keyMap: 'sublime',
+                mode: 'jsx',
+            }}
+            />
+        </div>
     )
 }
 
